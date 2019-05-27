3ab0847f4bb9d47d1754c810e73ab8f332b46823
Savio Andrade <saandrade@dundee.ac.uk>
2019-05-27 22:12:11 +0100

    added more functions to serve mode and altitude checking

--- DroneKit_Wrapper.py
+++ DroneKit_Wrapper.py
@@ -15,12 +15,14 @@ class Location:
     roll = None
     heading = None
     heartbeat=False
+    mode = None
 
     def __init__(self):
         try:
             self.vehicle = connect('/dev/ttyACM0', wait_ready=True, baud=57600, heartbeat_timeout=180)
             vehicle.home_location = vehicle.location.global_frame
             self.init_alt = self.vehicle.location._alt
+            self.mode = vehicle.mode.name
 
             print("\nConnected Successfully")
             print(" GPS: %s" % vehicle.gps_0)
@@ -39,7 +41,13 @@ class Location:
             print('Timeout!')
 
 
+    def check_mode(self):
+        self.mode = vehicle.mode.name
+        return self.mode
 
+    def get_altitude(self):
+        self.rel_alt = self.vehicle.location._alt - self.init_alt
+        return self.rel_alt
     def get_location(self):
         self.lat = self.vehicle.location._lat
         self.lon = self.vehicle.location._lon
