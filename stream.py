dfd430d3f466d2a33031dec08a5066bc94d45632
Savio Andrade <saandrade@dundee.ac.uk>
2019-05-27 20:46:33 +0100

    ready to stream data

--- stream.py
+++ stream.py
@@ -2,6 +2,14 @@
 
 import threading, image_cap, Database_Handler, DroneKit_Wrapper
 import time
+import gc
+import argparse
+
+gc.enable()
+
+#database modifier lock
+global database_mod
+database_mod = False
 
 global imgname
 global lat
@@ -13,9 +21,24 @@ global height
 
 stopped = threading.Event()
 
+#Sys Args via Argparse
+parser = argparse.ArgumentParser(description='Generates the dataset for the Haggis Target Detection Multiple Input-Output DCNN')
+parser.add_argument('-tlat', type=float, help="Define target latitude coordinates in degrees")
+parser.add_argument('-tlon', type=float, help="Define target longitude coordinates in degrees")
+parser.add_argument('-tchar', type=char, help="Define target character")
+args = parser.parse_args()
+
+
+#
+image_name_counter = 0
+
+#init database handler
+Database_Controller = Database_Handler.Database(args.tlat, args.tlon, args.tchar)
+time.sleep(5)
 
 #Continous DataStream
 Drone = DroneKit_Wrapper.Location()
+# TODO sleep until heartbeat is received
 time.sleep(5)
 
 def data_stream():
@@ -27,20 +50,35 @@ def data_stream():
         pitch = Drone.pitch
         roll = Drone.roll
         height = Drone.rel_alt
-        print(lat, lon, heading, pitch, roll, height)
+
+
+def database_write(image_name):
+    Database_Controller.collect(height, lat, lon, heading, pitch, roll, image_name)
+    processLock.accuire()
+    database_mod = False
+    processLock.release()
 
 
 def image_stream(counter):
     imgname = image_cap.get_img(counter)
-    #threading.Thread()
+    processLock.accuire()
+    database_mod = True
+    processLock.release()
+    dbThread = threading.Thread(target=database_write, args=(imgname))
+    dbThread.daemon=True
+    db.start()
 
 if __name__ == '__main__':
     try:
         global processLock
-        processlock = threading.Lock()
+        processLock = threading.Lock()
         dsThread = threading.Thread(target=data_stream)
         dsThread.start()
+        while True:
+            image_stream(image_name_counter)
+            image_name_counter+=1
         dsThread.join()
     except (KeyboardInterrupt, SystemExit):
         print("got Ctrl+C (SIGINT) or exit() is called")
         stopped.set() # signal threads to exit gracefully
+        Database_Controller.clean() # close database cleanly
